d75069806a1992b28f1ef6c7764ff9d791795543
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-11-28 12:21:50 +0100

    add custom registration form - AppBundle\Form\RegistrationType

---
+++ src/AppBundle/Form/RegistrationType.php
@@ -0,0 +1,29 @@
+<?php
+	
+	namespace AppBundle\Form;
+	
+	use Symfony\Component\Form\AbstractType;
+	use Symfony\Component\Form\FormBuilderInterface;
+	
+	class RegistrationType extends AbstractType {
+		public function buildForm(FormBuilderInterface $builder, array $options) {
+			$builder->add('name')
+			        ->add('surname')
+			        ->add('adress')
+			        ->add('description')
+			        ->add('region')
+			        ->add('city')
+			        ->add('phone');
+		}
+		
+		public function getParent() {
+			return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+		}
+		
+		public function getBlockPrefix() {
+			return 'app_user_registration';
+		}
+		
+	}
+	
+?>
