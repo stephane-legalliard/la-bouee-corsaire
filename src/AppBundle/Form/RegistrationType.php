4e025f8aaebb6cba70023e80202e08878a6e757b
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-11-28 13:53:44 +0100

    AppBundle\Form\RegistrationType - further customization of registration form

--- src/AppBundle/Form/RegistrationType.php
+++ src/AppBundle/Form/RegistrationType.php
@@ -2,24 +2,62 @@
 	
 	namespace AppBundle\Form;
 	
+	use FOS\UserBundle\Util\LegacyFormHelper;
 	use Symfony\Component\Form\AbstractType;
 	use Symfony\Component\Form\FormBuilderInterface;
+	use Symfony\Component\OptionsResolver\OptionsResolver;
 	
 	class RegistrationType extends AbstractType {
+		
+		/**
+		 * @var string
+		 */
+		private $class;
+		
+		/**
+		 * @param string $class The User class name
+		 */
+		public function __construct($class = 'AppBundle\Entity\User') {
+			$this->class = $class;
+		}
+		
+		/**
+		 * {@inheritdoc}
+		 */
 		public function buildForm(FormBuilderInterface $builder, array $options) {
-			$builder->add('name')
-			        ->add('surname')
-			        ->add('adress')
-			        ->add('description')
-			        ->add('region')
-			        ->add('city')
-			        ->add('phone');
+			$builder
+			 ->add('username', null, array('label' => 'Pseudonyme'))
+			 ->add('plainPassword', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\RepeatedType'), array(
+			 	'type' => LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'),
+			 	'options' => array('translation_domain' => 'FOSUserBundle'),
+			 	'first_options' => array('label' => 'Mot de passe'),
+			 	'second_options' => array('label' => 'Mot de passe (confirmation)'),
+			 	'invalid_message' => 'fos_user.password.mismatch',
+			 ))
+			 ->add('name', null, array('label' => 'Prénom'))
+			 ->add('surname', null, array('label' => 'Nom'))
+			 ->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'), array('label' => 'Adresse e-mail'))
+			 ->add('adress', null, array('label' => 'Adresse'))
+			 ->add('region', null, array('label' => 'Région'))
+			 ->add('city', null, array('label' => 'Ville'))
+			 ->add('phone', null, array('label' => 'Téléphone (optionnel)'))
+			 ->add('description', null, array('label' => 'Description'));
 		}
 		
-		public function getParent() {
-			return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+		/**
+		 * {@inheritdoc}
+		 */
+		public function configureOptions(OptionsResolver $resolver)
+		{
+			$resolver->setDefaults(array(
+				'data_class' => $this->class,
+				'csrf_token_id' => 'registration',
+			));
 		}
 		
+		/**
+		 * {@inheritdoc}
+		 */
 		public function getBlockPrefix() {
 			return 'app_user_registration';
 		}
