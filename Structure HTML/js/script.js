82d3b4367a639d68f1cb8a7a0573f70a13589549
Annie-Line <leclair.an@gmail.com>
2016-11-30 17:27:38 +0100

    Few problems

--- Structure HTML/js/script.js
+++ Structure HTML/js/script.js
@@ -1,18 +1,9 @@
 // Slide - HEADER
 
-$('.owl-carousel').owlCarousel({
+
+/*Appel plugin Owl-caroussel*/
+/*$('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
+    item: 4,
 })
