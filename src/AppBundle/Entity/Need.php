16c06d8b0423639c27d68c229dd60ec1aac50cf8
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-11-30 14:08:09 +0100

    $level management done in AppBundle\Entity\Need & AppBundle\Entity\Service instead of AppBundle\Entity\Task + use 'datetimetz' instead of 'datetime' for Doctrine date storage

--- src/AppBundle/Entity/Need.php
+++ src/AppBundle/Entity/Need.php
@@ -3,15 +3,54 @@
 	namespace AppBundle\Entity;
 	
 	use AppBundle\Entity\Task;
+	use AppBundle\DBAL\Types\NeedLevelType;
 	use Doctrine\ORM\Mapping as ORM;
-	//use Symfony\Component\Validator\Constraints as Assert;
+	use Fresh\DoctrineEnumBundle\Validator\Constraints as DoctrineAssert;
 	
 	/**
 	 * @ORM\Entity
 	 * @ORM\Table(name="needs")
 	 */
 	class Need extends Task {
-		//TODO
+		
+		/**
+		 * Level of the User providing the service
+		 *
+		 * @ORM\Column(type="NeedLevelType", nullable=false, options={"default"="0"})
+		 * @DoctrineAssert\Enum(entity="AppBundle\DBAL\Types\NeedLevelType")
+		 *
+		 * @var    enum $level
+		 * @access protected
+		 */
+		protected $level;
+		
+		/**
+		 * Get level
+		 *
+		 * @return string
+		 */
+		public function getLevel() { return $this->level; }
+		
+		/**
+		 * Set level
+		 *
+		 * @param string
+		 *
+		 * @return Task
+		 */
+		public function setLevel($level) {
+			switch ($level) {
+				case '0':
+				case '1':
+				case '2':
+				case '3':
+					$this->level = $level;
+					break;
+			}
+			
+			return $this;
+		}
+		
 	}
 	
 ?>
