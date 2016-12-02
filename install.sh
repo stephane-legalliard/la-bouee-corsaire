475911d40a4f84ac75bc34e6c505f629b45e0d48
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-12-02 13:27:20 +0100

    automatic generation of symbolic links

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -e
 
 ./composer.phar install
+ln --symbolic --force vendor/bin/npm .
 ./npm install
+ln --symbolic --force ../../node_modules app/Resources
 bin/console doctrine:schema:update --force
 
 exit 0
