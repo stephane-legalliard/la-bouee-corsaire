77f52c73e765fc013fd46bffc3c041dd4703674d
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-12-02 13:54:34 +0100

    drop dependency on uglifyjs and node.js
    Revert "Assetic - register a uglifyjs2 filter"
    This reverts commit 6071771552c1ef285cbe3e2335e078398483ef13.
    
    Revert "add dependencies installation by npm in ./install.sh"
    This reverts commit cb62922d2d1b43ff05fc4cc4fab26983c59b6862.
    
    Revert "add /node_modules/ to .gitignore"
    This reverts commit 1a4b985ea9e6fea84a6f094fea212bbac5bc9d94.
    
    Revert "link npm binary in root dir"
    This reverts commit 38f4f1f144f40c83cc08f8c11db45134c1e918c8.
    
    Revert "add dependency on mouf/nodejs-installer"
    This reverts commit c30d28e44501b99748e60c67d77334134498a12e.

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/bin/sh -e
 
 ./composer.phar install
-ln --symbolic --force vendor/bin/npm .
-./npm install
-ln --symbolic --force ../../node_modules app/Resources
 bin/console doctrine:schema:update --force
 
 exit 0
