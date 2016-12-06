5e5c3cc38e826e2785648c8346c8dabad18b2a0a
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-12-06 16:30:41 +0100

    Twig templates - merge login and registration forms

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -1,9 +1,15 @@
 <?php
-
-namespace AppBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class AppBundle extends Bundle
-{
-}
+	
+	namespace AppBundle;
+	
+	use Symfony\Component\HttpKernel\Bundle\Bundle;
+	
+	class AppBundle extends Bundle{
+		
+		public function getParent() {
+			return 'FOSUserBundle';
+		}
+		
+	}
+	
+?>
