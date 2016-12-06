7f25ba348fdc2280da4a3accbd1f7c9167f785ca
Annie-Line <leclair.an@gmail.com>
2016-12-06 11:50:13 +0100

    Adding modifications

--- Structure HTML/js/script.js
+++ Structure HTML/js/script.js
@@ -1,9 +1,12 @@
-// Slide - HEADER
+//Mettre le nom
 
-
-/*Appel plugin Owl-caroussel*/
-/*$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    item: 4,
-})
+jQuery(document).ready(function($){
+  $('.search-box input[type="text"]').focus(function(){
+    $('.search-filters').slideToggle();
+  });
+  $('.search-filters input[type="radio"]').on('click', function(){
+    var placeholder_text = $(this).closest('label').text();
+    $('.search-input').attr('placeholder', 'search: '+placeholder_text);
+    $('.search-filters').slideToggle();
+  });
+});
