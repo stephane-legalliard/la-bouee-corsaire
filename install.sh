f77ddd784538420c5a2bec7949fd2181f2ed30a9
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-12-08 14:34:15 +0100

    install and configure GrumPHP for automated tests

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 ./composer.phar install
 ln --symbolic --force vendor/bin/simple-phpunit ./phpunit
+ln --symbolic --force ./simple-phpunit vendor/bin/phpunit
 bin/console doctrine:schema:update --force
 
 exit 0
