8ebca275a65c5ca65d558da3f3948bf09c8d4d89
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-12-14 12:16:49 +0100

    new method AppBundle\Controller\AdminController::editAction + associated Twig template

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -2,9 +2,21 @@
 
 	namespace AppBundle\Controller;
 
+	use AppBundle\Entity\User;
+	use FOS\UserBundle\Event\FilterUserResponseEvent;
+	use FOS\UserBundle\Event\FormEvent;
+	use FOS\UserBundle\Event\GetResponseUserEvent;
+	use FOS\UserBundle\Form\Factory\FactoryInterface;
+	use FOS\UserBundle\FOSUserEvents;
+	use FOS\UserBundle\Model\UserInterface;
+	use FOS\UserBundle\Model\UserManagerInterface;
 	use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+	use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+	use Symfony\Component\HttpFoundation\RedirectResponse;
 	use Symfony\Component\HttpFoundation\Request;
+	use Symfony\Component\HttpFoundation\Response;
+	use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 	/**
 	 * Admin controller.
@@ -35,7 +47,9 @@
 		}
 		
 		/**
-		 *@Route("/users")
+		 *
+		 * @Route("/users")
+		 *
 		 */
 		public function listAction() {
 			$users = $this->get('fos_user.user_manager')
@@ -44,6 +58,69 @@
 				'users' => $users,
 			));
 		}
+
+		/**
+		 * Edit the user.
+		 *
+		 * @Route("/user/edit/{id}", name="user_edit")
+		 *
+		 * @param Request $request
+		 *
+		 * @return Response
+		 */
+		public function editAction(Request $request, $id) {
+			$user = $this->getDoctrine()
+			             ->getRepository('AppBundle:User')
+			             ->find($id);
+
+			if (!is_object($user) || !$user instanceof UserInterface) {
+				//TODO user not found page
+				throw $this->createNotFoundException(
+					'No User found for id '.$id
+				);
+			}
+
+			/** @var $dispatcher EventDispatcherInterface */
+			$dispatcher = $this->get('event_dispatcher');
+
+			$event = new GetResponseUserEvent($user, $request);
+			$dispatcher->dispatch(FOSUserEvents::PROFILE_EDIT_INITIALIZE, $event);
+
+			if (null !== $event->getResponse()) {
+				return $event->getResponse();
+			}
+
+			/** @var $formFactory FactoryInterface */
+			$formFactory = $this->get('fos_user.profile.form.factory');
+
+			$formFactory = $this->get('form.factory');
+			$form = $formFactory->createNamed('edit_user', 'AppBundle\Form\ProfileAdminType', $user);
+			$form->handleRequest($request);
+
+			if ($form->isSubmitted() && $form->isValid()) {
+				/** @var $userManager UserManagerInterface */
+				$userManager = $this->get('fos_user.user_manager');
+
+				$event = new FormEvent($form, $request);
+				$dispatcher->dispatch(FOSUserEvents::PROFILE_EDIT_SUCCESS, $event);
+
+				$userManager->updateUser($user);
+
+				if (null === $response = $event->getResponse()) {
+					$url = $this->generateUrl('user_show', array('id' => $id));
+					$response = new RedirectResponse($url);
+				}
+
+				$dispatcher->dispatch(FOSUserEvents::PROFILE_EDIT_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
+
+				return $response;
+			}
+
+			return $this->render('user/edit.admin.html.twig', array(
+				'form' => $form->createView(),
+			));
+		}
+
 	}
 
 ?>
---
+++ src/AppBundle/Form/ProfileAdminType.php
@@ -0,0 +1,84 @@
+<?php
+
+	namespace AppBundle\Form;
+
+	use FOS\UserBundle\Util\LegacyFormHelper;
+	use Symfony\Component\Form\AbstractType;
+	use Symfony\Component\Form\FormBuilderInterface;
+	use Symfony\Component\OptionsResolver\OptionsResolver;
+
+	class ProfileAdminType extends AbstractType {
+		/**
+		 * @var string
+		 */
+		private $class;
+
+		/**
+		 * @param string $class The User class name
+		 */
+		public function __construct($class = 'AppBundle\Entity\User') {
+			$this->class = $class;
+		}
+
+		/**
+		 * {@inheritdoc}
+		 */
+		public function buildForm(FormBuilderInterface $builder, array $options) {
+			$builder
+				->add('username', null, array(
+					'translation_domain' => 'FOSUserBundle',
+					'label' => 'form.username',
+				))
+				->add('name', null, array(
+					'translation_domain' => false,
+					'label' => 'Prénom',
+				))
+				->add('surname', null, array(
+					'translation_domain' => false,
+					'label' => 'Nom',
+				))
+				->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'),array(
+					'translation_domain' => 'FOSUserBundle',
+					'label' => 'form.email',
+				))
+				->add('adress', null, array(
+					'translation_domain' => false,
+					'label' => 'Adresse',
+				))
+				->add('region', null, array(
+					'translation_domain' => false,
+					'label' => 'Région',
+				))
+				->add('city', null, array(
+					'translation_domain' => false,
+					'label' => 'Ville',
+				))
+				->add('phone', null, array(
+					'translation_domain' => false,
+					'label' => 'Téléphone (optionnel)',
+				))
+				->add('description', null, array(
+					'translation_domain' => false,
+					'label' => 'Description',
+				));
+		}
+
+		/**
+		 * {@inheritdoc}
+		 */
+		public function configureOptions(OptionsResolver $resolver) {
+			$resolver->setDefaults(array(
+				'data_class' => $this->class,
+				'csrf_token_id' => 'profile',
+			));
+		}
+
+		/**
+		 * {@inheritdoc}
+		 */
+		public function getBlockPrefix() {
+			return 'fos_user_profile_admin';
+		}
+	}
+
+?>
