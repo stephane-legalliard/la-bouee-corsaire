6ae03391c0953d94ce88c96139784b873274d837
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-12-14 10:18:16 +0100

    Admin - new users list view + update AppBunlde\Controller\AdminController::listAction (formerly findUsers)

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -1,34 +1,26 @@
 <?php
 
-namespace AppBundle\Controller;
+	namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+	use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+	/**
+	 * User controller.
+	 *
+	 * @Route("/admin")
+	 */
+	class AdminController extends Controller {
+		/**
+		 *@Route("/users")
+		 */
+		public function listAction() {
+			$userManager = $this->get('fos_user.user_manager');
+			$users = $userManager->findUsers();
+			return $this->render('user/list.html.twig', array(
+				'users' => $users,
+			));
+		}
+	}
 
-/**
- * User controller.
- *
- * @Route("/admin")
- */
-
-
-class AdminController extends Controller
-{
-
-/**
-*@Route("/users")
-*/
-
-    public function findUsers()
-    {
-    	$userManager = $this->get('fos_user.user_manager');
-        $users = $userManager->findUsers();
-        return $this->render('user/list.html.twig', array(
-        	'users' => $users,
-        ));
-    }
-
-    	
-  	
-}
+?>
