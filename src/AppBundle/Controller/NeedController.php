stephane-legalliard/la-bouee-corsaire

commit 223164e0a39e4b3a4c8ba20203d643b51653ae00

AppBundle\Controller\NeedController::editAction - redirect to details of current Need after edition

src/AppBundle/Controller/NeedController.php
@@ -140,8 +140,7 @@
 				$need = $form->getData();
 				$em = $this->getDoctrine()->getManager();
 				$em->flush();
-				//TODO edit confirmation page
-				return new Response('<p>Saved modifications to Need with id '.$need->getId()."</p>\n<pre>".var_export($need, true).'</pre>');
+				return $this->redirectToRoute('need_show', array('id' => $id);
 			}
 			
 			return $this->render('need/edit.html.twig', array(

commit a593194c8723a9e5040879336af28be5c9dffef4

fix bug introduced by commit 223164e0a39e4b3a4c8ba20203d643b51653ae00

src/AppBundle/Controller/NeedController.php
@@ -140,7 +140,7 @@
 				$need = $form->getData();
 				$em = $this->getDoctrine()->getManager();
 				$em->flush();
-				return $this->redirectToRoute('need_show', array('id' => $id);
+				return $this->redirectToRoute('need_show', array('id' => $id));
 			}
 			
 			return $this->render('need/edit.html.twig', array(