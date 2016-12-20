65e8d7d01a309076ec90e30860c1a6ef7adeed7e
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-12-20 14:46:46 +0100

    AppBundle\Form\MessageType - duration field not required + add textarea type to content field

--- src/AppBundle/Form/MessageType.php
+++ src/AppBundle/Form/MessageType.php
@@ -6,7 +6,8 @@
 	use Symfony\Component\Form\FormBuilderInterface;
 	use Symfony\Component\OptionsResolver\OptionsResolver;
 	use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-	
+	use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
 	class MessageType extends AbstractType {
 		
 		/**
@@ -29,9 +30,10 @@
 				->add('duration', IntegerType::class, array(
 					'translation_domain' => false,
 					'label' => 'Durée estimée (en heures)',
-					'empty_data' => 0
+					'empty_data' => '0',
+					'required' => false,
 				))
-				->add('content', null, array(
+				->add('content', TextareaType::class, array(
 					'translation_domain' => false,
 					'label' => 'Rédigez votre message…'
 				));
