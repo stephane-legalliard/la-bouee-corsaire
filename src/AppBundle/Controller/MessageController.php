5f1bb76395c6334b6b35f06e83701e143fcf4e47
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-12-23 17:10:54 +0100

    AppBundle\Controller\MessageController - remove obsolete use statement

--- src/AppBundle/Controller/MessageController.php
+++ src/AppBundle/Controller/MessageController.php
@@ -10,7 +10,6 @@
 	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 	use Symfony\Component\HttpFoundation\Request;
 	use Symfony\Component\HttpFoundation\Response;
-	use Doctrine\Common\Collections\Criteria;
 
 	/**
 	 * Message controller.
