da65c15b1159dadd85057357a4d3b8c3a2a00875
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-12-23 11:25:25 +0100

    AppBundle\Controller\AdminUserController - remove obsolete use statements

--- src/AppBundle/Controller/AdminUserController.php
+++ src/AppBundle/Controller/AdminUserController.php
@@ -2,21 +2,15 @@
 
 	namespace AppBundle\Controller;
 
-	use AppBundle\Entity\User;
 	use FOS\UserBundle\Event\FilterUserResponseEvent;
 	use FOS\UserBundle\Event\FormEvent;
 	use FOS\UserBundle\Event\GetResponseUserEvent;
-	use FOS\UserBundle\Form\Factory\FactoryInterface;
 	use FOS\UserBundle\FOSUserEvents;
-	use FOS\UserBundle\Model\UserInterface;
-	use FOS\UserBundle\Model\UserManagerInterface;
 	use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-	use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 	use Symfony\Component\HttpFoundation\RedirectResponse;
 	use Symfony\Component\HttpFoundation\Request;
 	use Symfony\Component\HttpFoundation\Response;
-	use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 	/**
 	 * Admin Users controller.
