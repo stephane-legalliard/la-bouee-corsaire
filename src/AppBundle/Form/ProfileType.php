feacb80920ec92c3fd2d4a7341e2d7e1d24897d1
Antoine Le Gonidec <vv221@dotslashplay.it>
2017-01-02 15:21:46 +0100

    AppBundle\Form\ProfileType - Improve comments + Drop unused method getBlockPrefix()

--- src/AppBundle/Form/ProfileType.php
+++ src/AppBundle/Form/ProfileType.php
@@ -5,23 +5,21 @@
 	use Symfony\Component\OptionsResolver\OptionsResolver;
 	use AppBundle\Form\UserType;
 
+	/**
+	 * Form used for User edition by the account owner
+	 */
 	class ProfileType extends UserType {
 
 		/**
-		 * {@inheritdoc}
+		 * Configures the options for this type.
+		 *
+		 * @param OptionsResolver $resolver The resolver for the options
 		 */
 		public function configureOptions(OptionsResolver $resolver) {
 			$resolver->setDefaults(array(
 				'data_class' => $this->class,
 				'csrf_token_id' => 'profile',
 			));
-		}
-
-		/**
-		 * {@inheritdoc}
-		 */
-		public function getBlockPrefix() {
-			return 'fos_user_profile';
 		}
 
 	}
