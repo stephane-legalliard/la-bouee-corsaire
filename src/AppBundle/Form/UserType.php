ab5844e6c131b7420aa0ed8eb4216b5aa5a59275
Antoine Le Gonidec <vv221@dotslashplay.it>
2017-01-02 15:23:01 +0100

    AppBundle\Form\RegistrationType - Improve comments + Minor syntax tweaks

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -6,14 +6,21 @@
 	use Symfony\Component\Form\FormBuilderInterface;
 	use FOS\UserBundle\Util\LegacyFormHelper;
 
+	/**
+	 * Base used by all forms related to Users creation/edition
+	 */
 	abstract class UserType extends AbstractType {
 
 		/**
+		 * Associated class
+		 *
 		 * @var string
 		 */
 		protected $class;
 
 		/**
+		 * Contruct, set the associated class
+		 *
 		 * @param string $class The User class name
 		 */
 		public function __construct($class = 'AppBundle\Entity\User') {
@@ -21,38 +28,44 @@
 		}
 
 		/**
-		 * {@inheritdoc}
+		 * Builds the form.
+		 *
+		 * This method is called for each type in the hierarchy starting from the
+		 * top most type. Type extensions can further modify the form.
+		 *
+		 * @param FormBuilderInterface $builder The form builder
+		 * @param array                $options The options
 		 */
 		public function buildForm(FormBuilderInterface $builder, array $options) {
 			$builder
-				->add('name', null, array(
+				->add('name', null, [
 					'translation_domain' => false,
 					'label' => 'Prénom',
-				))
-				->add('surname', null, array(
+				])
+				->add('surname', null, [
 					'translation_domain' => false,
 					'label' => 'Nom',
-				))
-				->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'),array(
+				])
+				->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'),[
 					'translation_domain' => 'FOSUserBundle',
 					'label' => 'form.email',
-				))
-				->add('adress', null, array(
+				])
+				->add('adress', null, [
 					'translation_domain' => false,
 					'label' => 'Adresse',
-				))
-				->add('region', null, array(
+				])
+				->add('region', null, [
 					'translation_domain' => false,
 					'label' => 'Région',
-				))
-				->add('city', null, array(
+				])
+				->add('city', null, [
 					'translation_domain' => false,
 					'label' => 'Ville',
-				))
-				->add('phone', null, array(
+				])
+				->add('phone', null, [
 					'translation_domain' => false,
 					'label' => 'Téléphone (optionnel)',
-				));
+				]);
 			}
 
 	}
