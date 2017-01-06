9d87715c8c78aecf677373cc1064f0cc76d6e5e8
Antoine Le Gonidec <vv221@dotslashplay.it>
2017-01-06 13:32:03 +0100

    Remove composer.phar from tracked file + Add automatic download of composer.phar to install.sh operations

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
 #!/bin/sh -e
 
+if [ ! -e ./composer.phar ]; then
+	echo "Installation de composer…"
+	EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+	ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+
+	if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]; then
+		>&2 echo 'ERROR: Invalid installer signature'
+		rm composer-setup.php
+		exit 1
+	fi
+
+	php composer-setup.php --quiet
+	rm composer-setup.php
+fi
+
 ./composer.phar install
 ln --symbolic --force vendor/bin/simple-phpunit ./phpunit
 ln --symbolic --force ./simple-phpunit vendor/bin/phpunit
